0475466c515f9639bdc7889a95f0e9d47e2c58e3
Andres Guevara <aguevara19@uc.cl>
2022-10-04 13:19:04 -0300

    progreso en replit hasta 4 octubre

---
+++ script.js
@@ -0,0 +1,44 @@
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<select id="searchInput">
+<option value="all">all</option>
+    <option value="cat">cat</option>
+    <option value="dog">dog</option>
+    <option value="mouse">mouse</option>
+    </select>
+    <br/>
+    <table>
+    <thead>
+        <tr>
+            <th>Column1</th>
+            <th>Column2</th>
+        </tr>
+    </thead>
+    <tbody id="fbody">
+        <tr>
+            <td>cat</td>
+            <td>one</td>
+        </tr>
+        <tr>
+            <td>dog</td>
+            <td>two</td>
+        </tr>
+        <tr>
+            <td>cat</td>
+            <td>three</td>
+        </tr>
+        <tr>
+            <td>mouse</td>
+            <td>four</td>
+        </tr>
+        <tr>
+            <td>mouse</td>
+            <td>five</td>
+        </tr>
+        <tr>
+            <td>dog</td>
+            <td>six</td>
+        </tr>
+    </tbody>
+    </table>
+
